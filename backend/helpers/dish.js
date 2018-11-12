cb79a4e5a1602672337e688e50831ae1ee68fbef
Roberto Meran <romedu1@hotmail.com>
2018-11-12 05:08:08 +0000

    Update the Dish's helpers

--- backend/helpers/dish.js
+++ backend/helpers/dish.js
@@ -1,8 +1,14 @@
-const {Dish} = require("../models");
+const {Dish} = require("../models"),
+      {createError} = require("./error");
 
 exports.find = (req, res, next) => {
-    Dish.find({servedIn: req.params.id})
-        .then(dishes => res.status(200).json(dishes))
+    const searchQuery = req.params.all === "true" ? {} : {servedIn: req.params.id};
+    
+    Dish.find(searchQuery)
+        .then(dishes =>{
+            if(!dishes) throw createError(404, "Not Found");
+            return res.status(200).json(dishes);
+        })
         .catch(error => next(error));
 };
 
@@ -10,7 +16,7 @@ exports.create = (req, res, next) => {
     Dish.create(req.body)
         .then(newDish => {
             const {restaurant} = req;
-            restaurant.menu.push(newDish);
+            restaurant.menu.push(newDish._id);
             newDish.servedIn = restaurant._id;
             return Promise.all([newDish, restaurant.save(), newDish.save()]);
         })
@@ -22,14 +28,17 @@ exports.create = (req, res, next) => {
 };
 
 exports.findOne = (req, res, next) => {
-    Dish.findById(req.params.dishId)
-        .then(dish => res.status(200).json(dish))
+    Dish.findById(req.params.dishId).populate("servedIn").exec()
+        .then(dish =>{
+            if(!dish) throw createError(404, "Not Found");
+            return res.status(200).json(dish);
+        })
         .catch(error => next(error));
 };
 
 exports.update = (req, res, next) => {
     Dish.findByIdAndUpdate(req.params.dishId, req.body, {new: true})
-        .then(editDish => res.status(204).json(editDish))
+        .then(editedDish => res.status(200).json(editedDish))
         .catch(error => {
             error.status = 409; 
             return next(error);
@@ -40,10 +49,10 @@ exports.delete = (req, res, next) => {
     Dish.findByIdAndRemove(req.params.dishId)
         .then(exDish => {
             const {restaurant} = req;
-            restaurant.menu.pull(exDish.id);
+            restaurant.menu.pull(exDish._id);
             return restaurant.save();
         })
-        .then(resolve => res.status(204).json({message: "Dish removed successfully"}))
+        .then(resolve => res.status(200).json({message: "Dish removed successfully"}))
         .catch(error => next(error));
 };
 
