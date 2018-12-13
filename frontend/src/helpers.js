883d9a14cb9b6e37547e293d2dc51e9dcde5fbf0
romedu <32285662+romedu@users.noreply.github.com>
2018-12-12 21:43:43 -0400

    Add new helpers functions

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -1,3 +1,5 @@
 export const findById = (itemId, itemList) => itemList.find(item => item._id === itemId);
 export const updateItem = (itemId, newItem, itemList) => itemList.map(item => item._id === itemId ? newItem : item);
 export const deleteItem = (itemId, itemList) => itemList.filter(item => item._id !== itemId);
+export const capitalizeString = phrase =>  phrase.toLowerCase().split(" ").map(word => word.split("").map((letter, index) => index === 0 ? letter.toUpperCase() : letter).join("")).join(" ");
+export const kebabCaseString = phrase => phrase.toLowerCase().split(" ").join("-");
