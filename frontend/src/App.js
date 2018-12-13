b03fa95a5bd78bd1392be1a294a1f4eea2d5819a
romedu <32285662+romedu@users.noreply.github.com>
2018-12-12 22:05:49 -0400

    Update to support the navigation refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,13 @@ import {connect} from "react-redux";
 import Navigation from "./Containers/Navigation/Navigation";
 import Hotel from "./Containers/Hotel/Hotel";
 import * as actions from "./store/actions/user";
-import * as actionTypes from "./store/actions/actionTypes";
 import './App.css';
 
 class App extends Component {
-  state = {
-    showSideDrawer: false
-  }
-
   componentDidMount(){
     const token = localStorage.getItem("token");
     if(token) return this.props.onTokenVerify(token);
   }
-
-  showSideDrawerHandler = () => this.setState(prevState => ({showSideDrawer: !prevState.showSideDrawer}));
-  hideSideDrawerHandler = () => this.setState(prevState => ({showSideDrawer: false}));
   
   render() {
     return (
@@ -42,7 +34,6 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   onLoginHandler: (authType, body) => dispatch(actions.loginUser(authType, body)),
-  onLogoutHandler: () => dispatch({type: actionTypes.LOGOUT}),
   onTokenVerify: token => dispatch(actions.verifyToken(token))
 });
 
